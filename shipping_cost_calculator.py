195862da8b5743c4b258965c8538630636d9ed7d
thomasnguyen02 <thomas.l.nguyen02@gmail.com>
2026-04-01 15:47:54 -0700

    Update shipping_cost_calculator.py

--- shipping_cost_calculator.py
+++ shipping_cost_calculator.py
@@ -1,39 +1,30 @@
-#Input
-weight = 400
+weight = 1
 
-#Ground Shipping 
+# Ground Shipping 
 if weight <= 2: 
   ground_cost = (weight * 1.50) + 20.00
-  print("Ground Shipping: ", ground_cost)
-elif weight > 2 and weight <= 6: 
+elif weight <= 6: 
   ground_cost = (weight * 3.00) + 20.00
-  print("Ground Shipping: ", ground_cost) 
-elif weight > 6 and weight <= 10: 
+elif weight <= 10: 
   ground_cost = (weight * 4.00) + 20.00
-  print("Ground Shipping: ", ground_cost)
 else: 
-  ground_cost = (weight * 4.75) + 20.00
-  print("Ground Shipping: ", ground_cost)
+  ground_cost = (weight * 4.75) + 20.00 
+print("Ground Shipping Cost:", ground_cost)
 
-#Ground Shipping Premium 
-premium_cost = 125
-print("Ground Shipping Premium: ", premium_cost)
+# Ground Shipping Premium 
+premium_cost = 125.00
+print("Ground Shipping Premium Cost:", premium_cost)
 
-#Drone Shipping
+# Drone Shipping
 if weight <= 2: 
   drone_cost = (weight * 4.50)
-  print("Drone Shipping: ", drone_cost)
-elif weight > 2 and weight <= 6: 
+elif weight <= 6: 
   drone_cost = (weight * 9.00)
-  print("Drone Shipping: ", drone_cost)
-elif weight > 6 and weight <= 10: 
+elif weight <= 10: 
   drone_cost = (weight * 12.00)
-  print("Drone Shipping: ", drone_cost)
 else: 
   drone_cost = (weight * 14.25)
-  print("Drone Shipping: ", drone_cost)
+print("Drone Shipping Cost:", drone_cost)
 
-#Output
 cheapest = min(ground_cost, premium_cost, drone_cost)
-print("Cheapest Option: ", "$", cheapest)
-
+print("Cheapest Option: $", cheapest)
